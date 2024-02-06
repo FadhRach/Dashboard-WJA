e46765c8d487c6f8a0820a4fcf3364b9ade403e9
Fadhlan <fadhlannur007@gmail.com>
2024-02-06 14:48:54 +0700

    coba ubah tampilan backup kecommit sini

--- index.php
+++ index.php
@@ -26,6 +26,9 @@
         <!-- Custom Fonts -->
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+        <!-- Tailwind CSS -->
+        <script src="https://cdn.tailwindcss.com"></script>
+
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
